76c14aee3a20e9a774e4c7009c4a8840f8ce6811
cyberghosts <yee.darren@gmail.com>
2018-05-26 21:56:57 -0600

    Signed-off-by: cyberghosts <yee.darren@gmail.com>

--- app/config.js
+++ app/config.js
@@ -1,26 +1,9 @@
-// Sets the MongoDB Database options
-//BigCoder:BigCoder!@ds139817.mlab.com:39817/heroku_cnkn5vpn
-
 module.exports = {
 
     mongolab:
     {
         name: "mongolab",
         url: "mongodb://ymmccd:ymmccd@ds235840.mlab.com:35840/ymmccd",
-        port: 27017
-    },
-
-    local:
-    {
-        name: "scotch-user-map-local",
-        url: "mongodb://localhost/MeanMapApp",
-        port: 27017
-    },
-
-    localtest:
-    {
-        name: "scotch-user-map-local",
-        url: "mongodb://localhost/MeanMapAppTest",
         port: 27017
     }
 
